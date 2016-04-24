8cf253210c6b78f2ee6f928d2eff12556ac178a0
Sokovikov <skv-headless@yandex.ru>
2016-04-24 10:52:24 +0300

    call

--- player.rb
+++ player.rb
@@ -1,11 +1,11 @@
+require 'pry'
 
 class Player
 
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    small_blind = game_state["small_blind"]
-    small_blind * 2
+    game_state["current_buy_in"] - game_state["players"][game_state["in_action"]]["bet"] + 1
   end
 
   def showdown(game_state)
