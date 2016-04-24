bf548298d7b8ab0356e00c59a99fd330ca5fe142
Vagin Alexander <laplugin73@gmail.com>
2016-04-24 10:41:33 +0400

    raise bet

--- player.rb
+++ player.rb
@@ -4,7 +4,8 @@ class Player
   VERSION = "Default Ruby folding player"
 
   def bet_request(game_state)
-    0
+    small_blind = game_state["small_blind"]
+    small_blind * 2
   end
 
   def showdown(game_state)
