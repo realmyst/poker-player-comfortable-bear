8bbe806fa2bb57244489d99133a0fcbd39b57d67
Alexander Shcherbinin <realmyst@ya.ru>
2016-04-24 10:50:48 +0400

    add have pair check

--- player.rb
+++ player.rb
@@ -9,6 +9,20 @@ class Player
   end
 
   def showdown(game_state)
+  end
 
+  def check_combination(player_cards, community_cards = [])
+  end
+
+  def have_pair?(player_cards, community_cards = [])
+    card1 = player_cards[0]
+    card2 = player_cards[1]
+    community_ranks = community_cards.map {|c| c["rank"] }
+
+    return true if card1["rank"] == card2["rank"]
+    return true if community_ranks.include? card1["rank"]
+    return true if community_ranks.include? card2["rank"]
+
+    false
   end
 end
